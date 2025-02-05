cb43ffb1a8d216a3a02771e5f6eb0ab2bfb28aa4
mr.malekey <mr.malekey@gmail.com>
2025-02-05 16:36:43 +0330

    updated proxy

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"crypto/tls"
 	"encoding/base64"
 	"io"
 	"log"
@@ -11,12 +12,19 @@ import (
 	"strings"
 )
 
-var proxyAddr = ":4040"
-var user = "user"
-var password = "password"
+var (
+	proxyAddr     = ":4040"
+	user          = "user"
+	password      = "password"
+	enableSSL      = false
+	certFile       = "server.crt"
+	keyFile        = "server.key"
+	allowedIPs     = []string{"127.0.0.1"}
+)
 
 func main() {
-	if tmp := os.Getenv("PROXY_USER"); tmp != "" {
+	// Load configuration from environment variables
+	if tmp := os.Getenv("PROXY_ADDR"); tmp != "" {
 		proxyAddr = tmp
 	}
 	if tmp := os.Getenv("PROXY_USER"); tmp != "" {
@@ -25,14 +33,43 @@ func main() {
 	if tmp := os.Getenv("PROXY_PASSWORD"); tmp != "" {
 		password = tmp
 	}
+	if tmp := os.Getenv("ENABLE_SSL"); tmp == "true" {
+		enableSSL = true
+	}
+	if tmp := os.Getenv("CERT_FILE"); tmp != "" {
+		certFile = tmp
+	}
+	if tmp := os.Getenv("KEY_FILE"); tmp != "" {
+		keyFile = tmp
+	}
+	if tmp := os.Getenv("ALLOWED_IPS"); tmp != "" {
+		allowedIPs = strings.Split(tmp, ",")
+	}
 
-	listener, err := net.Listen("tcp", proxyAddr)
+	var listener net.Listener
+	var err error
+
+	if enableSSL {
+		log.Println("Starting proxy with SSL termination")
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			log.Fatalf("Failed to load SSL certificate and key: %v", err)
+		}
+
+		config := &tls.Config{Certificates: []tls.Certificate{cert}}
+		listener, err = tls.Listen("tcp", proxyAddr, config)
+	} else {
+		log.Println("Starting proxy without SSL termination")
+		listener, err = net.Listen("tcp", proxyAddr)
+	}
+
 	if err != nil {
 		log.Fatalf("Failed to start proxy: %v", err)
 	}
 	defer listener.Close()
 
-	log.Printf("Proxy server is running on %s\n", proxyAddr)
+	log.Printf("Proxy server is running on %s
+", proxyAddr)
 
 	for {
 		clientConn, err := listener.Accept()
@@ -48,28 +85,49 @@ func handleClient(clientConn net.Conn) {
 	defer clientConn.Close()
 
 	clientAddr := clientConn.RemoteAddr().String()
-	log.Printf("New connection from: %s\n", clientAddr)
+	log.Printf("New connection from: %s
+", clientAddr)
+
+	// IP filtering check
+	clientIP := strings.Split(clientAddr, ":")[0]
+	if !isAllowedIP(clientIP) {
+		log.Printf("Connection from %s denied (IP not allowed)", clientIP)
+		clientConn.Write([]byte("HTTP/1.1 403 Forbidden
+
+"))
+		return
+	}
 
 	reader := bufio.NewReader(clientConn)
 	request, err := http.ReadRequest(reader)
 	if err != nil {
-		log.Printf("Failed to read request from %s: %v\n", clientAddr, err)
+		log.Printf("Failed to read request from %s: %v", clientAddr, err)
 		return
 	}
 
 	if !authenticate(request) {
-		clientConn.Write([]byte("HTTP/1.1 407 Proxy Authentication Required\r\nProxy-Authenticate: Basic realm=\"Proxy\"\r\n\r\n"))
-		log.Printf("Authentication failed for %s\n", clientAddr)
+		clientConn.Write([]byte("HTTP/1.1 407 Proxy Authentication Required
+Proxy-Authenticate: Basic realm=\"Proxy\"\r\n\r\n"))
+		log.Printf("Authentication failed for %s", clientAddr)
 		return
 	}
 
-	log.Printf("Request from %s: %s %s\n", clientAddr, request.Method, request.URL)
+	log.Printf("Request from %s: %s %s", clientAddr, request.Method, request.URL)
 
 	if request.Method == http.MethodConnect {
 		handleConnect(clientConn, request)
 	} else {
 		handleHTTP(clientConn, request)
 	}
+}
+
+func isAllowedIP(clientIP string) bool {
+	for _, allowedIP := range allowedIPs {
+		if clientIP == allowedIP {
+			return true
+		}
+	}
+	return false
 }
 
 func authenticate(request *http.Request) bool {
@@ -102,11 +160,11 @@ func handleConnect(clientConn net.Conn, request *http.Request) {
 		targetAddr += ":443"
 	}
 
-	log.Printf("CONNECT request to: %s\n", targetAddr)
+	log.Printf("CONNECT request to: %s", targetAddr)
 
 	targetConn, err := net.Dial("tcp", targetAddr)
 	if err != nil {
-		log.Printf("Failed to connect to target %s: %v\n", targetAddr, err)
+		log.Printf("Failed to connect to target %s: %v", targetAddr, err)
 		clientConn.Write([]byte("HTTP/1.1 502 Bad Gateway\r\n\r\n"))
 		return
 	}
@@ -124,11 +182,11 @@ func handleHTTP(clientConn net.Conn, request *http.Request) {
 		targetAddr += ":80"
 	}
 
-	log.Printf("HTTP request to: %s\n", targetAddr)
+	log.Printf("HTTP request to: %s", targetAddr)
 
 	targetConn, err := net.Dial("tcp", targetAddr)
 	if err != nil {
-		log.Printf("Failed to connect to target %s: %v\n", targetAddr, err)
+		log.Printf("Failed to connect to target %s: %v", targetAddr, err)
 		clientConn.Write([]byte("HTTP/1.1 502 Bad Gateway\r\n\r\n"))
 		return
 	}
@@ -136,9 +194,10 @@ func handleHTTP(clientConn net.Conn, request *http.Request) {
 
 	err = request.Write(targetConn)
 	if err != nil {
-		log.Printf("Failed to forward request to %s: %v\n", targetAddr, err)
+		log.Printf("Failed to forward request to %s: %v", targetAddr, err)
 		return
 	}
 
 	io.Copy(clientConn, targetConn)
 }
+
